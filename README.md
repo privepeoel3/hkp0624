350b2b655d3a0bab309d15bf7afd42e36850a2bc
privepeoel3 <106164687+privepeoel3@users.noreply.github.com>
2022-06-24 09:37:17 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,52 @@
+﻿
+
+## HKP0624
+
+[![](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy?template=https://github.com/privepeoel3/check0624.git)
+
+
+
+### 反代代码
+
+<details>
+<summary>CloudFlare Pages单账户反代代码</summary>
+
+```js
+export default {
+  async fetch(request, env) {
+    let url = new URL(request.url);
+    if (url.pathname.startsWith('/')) {
+      url.hostname = 'app0.herokuapp.com'
+      let new_request = new Request(url, request);
+      return fetch(new_request);
+    }
+    return env.ASSETS.fetch(request);
+  },
+};
+```
+</details>
+
+<details>
+<summary>CloudFlare Pages单双日轮换反代代码</summary>
+
+```js
+export default {
+  async fetch(request, env) {
+    const day1 = 'app0.herokuapp.com'
+    const day2 = 'app1.herokuapp.com'
+    let url = new URL(request.url);
+    if (url.pathname.startsWith('/')) {
+      let day = new Date()
+      if (day.getDay() % 2) {
+        url.hostname = day1
+      } else {
+        url.hostname = day2
+      }
+      let new_request = new Request(url, request);
+      return fetch(new_request);
+    }
+    return env.ASSETS.fetch(request);
+  },
+};
+```
+</details>
---
+++ entrypoint.sh
@@ -0,0 +1,67 @@
+﻿#!/bin/sh
+ 
+if [[ -z "${Vless_Path}" ]]; then
+  Vless_Path="vlok"
+fi
+echo ${Vless_Path}
+
+if [[ -z "${Vless_UUID}" ]]; then
+  Vless_UUID="f8bfb621-6728-4a6c-ae69-2106cd3d7c8a"
+fi
+echo ${Vless_UUID}
+
+if [[ -z "${Vmess_Path}" ]]; then
+  Vmess_Path="vmok"
+fi
+echo ${Vmess_Path}
+
+if [[ -z "${Vmess_UUID}" ]]; then
+  Vmess_UUID="f8bfb621-6728-4a6c-ae69-2106cd3d7c8a"
+fi
+echo ${Vmess_UUID}
+
+
+mkdir /xraybin
+cd /xraybin
+RAY_URL="https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip"
+echo ${RAY_URL}
+wget --no-check-certificate ${RAY_URL}
+unzip Xray-linux-64.zip
+rm -f Xray-linux-64.zip
+chmod +x ./xray
+ls -al
+
+# cd /wwwroot
+# tar xvf wwwroot.tar.gz
+# rm -rf wwwroot.tar.gz
+
+# Install Html
+mkdir /wwwroot
+wget -qO /tmp/html.zip ${Site} 
+unzip -qo /tmp/html.zip -d /wwwroot 
+rm -rf /tmp/html.zip
+
+sed -e "/^#/d"\
+    -e "s/\${Vless_UUID}/${Vless_UUID}/g"\
+    -e "s|\${Vless_Path}|${Vless_Path}|g"\
+    -e "s/\${Vmess_UUID}/${Vmess_UUID}/g"\
+    -e "s|\${Vmess_Path}|${Vmess_Path}|g"\
+    /conf/Xray.template.json >  /xraybin/config.json
+echo /xraybin/config.json
+cat /xraybin/config.json
+
+
+sed -e "/^#/d"\
+    -e "s/\${PORT}/${PORT}/g"\
+    -e "s|\${Vless_Path}|${Vless_Path}|g"\
+    -e "s|\${Vmess_Path}|${Vmess_Path}|g"\
+    -e "$s"\
+    /conf/nginx.template.conf > /etc/nginx/conf.d/ray.conf
+echo /etc/nginx/conf.d/ray.conf
+cat /etc/nginx/conf.d/ray.conf
+
+cd /xraybin
+./xray run -c ./config.json &
+rm -rf /etc/nginx/sites-enabled/default
+nginx -g 'daemon off;'
+
